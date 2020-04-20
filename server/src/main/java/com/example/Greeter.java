2e63a5edf77d730a0998f9496abd71a4e92d2e86
ocalsin <ocalsin2015@gmail.com>
2020-04-19 20:05:38 -0500

    correcting the error

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,12 @@ public class Greeter {
   }
 
 
-  public final String greet(final String someone) {
+ 	/**
+	 * example java document
+	 * @param someone
+	 * @return
+	 */
+ public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
